327ae840948353bc2e125d0ede6143415e853f90
HamzaLatif02 <hamza.latif@city.ac.uk>
2022-02-04 14:50:12 +0000

    code encapsulation

---
+++ src/game/GameWorld.java
@@ -0,0 +1,26 @@
+package game;
+
+import city.cs.engine.BoxShape;
+import city.cs.engine.Shape;
+import city.cs.engine.StaticBody;
+import city.cs.engine.World;
+import org.jbox2d.common.Vec2;
+
+public class GameWorld extends World {
+    public GameWorld() {
+        super();
+
+        Shape shape = new BoxShape(11, 0.5f);
+        StaticBody ground = new StaticBody(this, shape);
+        ground.setPosition(new Vec2(0f, -11.5f));
+
+        Shape platformShape = new BoxShape(3, 0.5f);
+        StaticBody platform1 = new StaticBody(this, platformShape);
+        platform1.setPosition(new Vec2(-8, -4f));
+
+        Student student = new Student(this);
+        student.setPosition(new Vec2(7, -9f));
+        student.setCredits(15);
+
+    }
+}
