f903e5d6d3a3be67bcf64194967af25564da791e
HamzaLatif02 <hamza.latif@city.ac.uk>
2022-03-02 15:21:13 +0000

    update character shape

--- src/game/PolygonEditor.java
+++ src/game/PolygonEditor.java
@@ -241,8 +241,8 @@ public class PolygonEditor extends JPanel
      * @param args command line arguments.
      */
     public static void main(String[] args) {
-        String fileName = "data/student.png";
-        float boxHeight = 4.0f;
+        String fileName = "data/character-left.png";
+        float boxHeight = 6.0f;
         int firstCoordIndex = 0;
         if (args.length > 0) {
             try {
