4ace95f5beeec2d19558ae9177d3990487183eee
HamzaLatif02 <hamza.latif@city.ac.uk>
2022-03-08 17:27:56 +0000

    update character images when moving left/right

--- src/game/Character.java
+++ src/game/Character.java
@@ -5,7 +5,7 @@ import city.cs.engine.*;
 public class Character extends Walker {
     //private static final Shape characterShape = new CircleShape(2);
     private static final Shape characterShape = new PolygonShape(0.71f,1.92f, 2.2f,1.32f, 1.57f,-2.04f, -1.27f,-1.97f, -2.17f,-0.42f, -2.02f,0.82f, -1.32f,1.7f, -0.2f,2.06f);
-    private static final BodyImage image = new BodyImage("data/character-left.png", 6f);
+    private static final BodyImage image = new BodyImage("data/character-right.png", 6f);
 
     private int points, health;
 
@@ -14,7 +14,7 @@ public class Character extends Walker {
         addImage(image);
         this.points = 0;
         this.health = 100;
-        //setAlwaysOutline(true);
+        setAlwaysOutline(true);
     }
 
     public int getPoints() {
--- src/game/CharacterController.java
+++ src/game/CharacterController.java
@@ -1,5 +1,7 @@
 package game;
 
+import city.cs.engine.BodyImage;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -18,8 +20,12 @@ public class CharacterController implements KeyListener {
         // other key commands omitted
         if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A) {
             character.startWalking(-WALKING_SPEED);
+            character.removeAllImages();
+            character.addImage(new BodyImage("data/character-left-flame.png",6f));
         } else if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
             character.startWalking(WALKING_SPEED);
+            character.removeAllImages();
+            character.addImage(new BodyImage("data/character-right-flame.png",6f));
         } else if (code == KeyEvent.VK_UP || code == KeyEvent.VK_W){
             character.jump(15f);
         }
@@ -30,8 +36,12 @@ public class CharacterController implements KeyListener {
         int code = e.getKeyCode();
         if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A) {
             character.stopWalking();
+            character.removeAllImages();
+            character.addImage(new BodyImage("data/character-left.png",6f));
         } else if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
             character.stopWalking();
+            character.removeAllImages();
+            character.addImage(new BodyImage("data/character-right.png",6f));
         }
     }
 
